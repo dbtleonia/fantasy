ff2cd686750de769dee7c211a2a7fcf4af862277
David B. Tucker <dbtleonia@gmail.com>
2016-08-19 21:31:37 -0400

    Add min & max positions to genrules.

--- genrules/genrules.go
+++ genrules/genrules.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-func allowedPos(schema, priorityStarters, roster []byte) string {
+func allowedPos(schema, priorityStarters, posMin, posMax, roster []byte) string {
 	starters := make(map[byte]int)
 	startersCount := 0
 	for _, ch := range schema {
@@ -56,24 +56,73 @@ func allowedPos(schema, priorityStarters, roster []byte) string {
 		}
 	}
 
-	return "DKQRTW"
+	// Rule #3: Fill position minimums by end of draft.
+	needMin := make(map[byte]int)
+	needMinCount := 0
+	for _, pos := range posMin {
+		needMin[pos]++
+		needMinCount++
+	}
+	for _, pos := range roster {
+		if needMin[pos] > 0 {
+			needMin[pos]--
+			needMinCount--
+		}
+	}
+	if len(roster)+needMinCount >= len(schema) {
+		var allowedMin string
+		for _, pos := range []byte("DKQRTW") {
+			if needMin[pos] > 0 {
+				allowedMin += string(pos)
+			}
+		}
+		return allowedMin
+	}
+
+	// Rule #4: Filter out positions that already reached maximum.
+	leftMax := make(map[byte]int)
+	for _, pos := range posMax {
+		leftMax[pos]++
+	}
+	for _, pos := range roster {
+		if leftMax[pos] > 0 {
+			leftMax[pos]--
+		}
+	}
+	var allowedMax string
+	for _, pos := range []byte("DKQRTW") {
+		if leftMax[pos] > 0 {
+			allowedMax += string(pos)
+		}
+	}
+	return allowedMax
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatalf("Usage: %s <combos-file> <schema>", os.Args[0])
+	if len(os.Args) != 7 {
+		log.Fatalf("Usage: %s <combos-file> <schema> <autopick-min> <autopick-max> <humanoid-min> <humanoid-max>", os.Args[0])
 	}
 	f, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	schema := []byte(os.Args[2])
+	var (
+		schema      = []byte(os.Args[2])
+		autopickMin = []byte(os.Args[3])
+		autopickMax = []byte(os.Args[4])
+		humanoidMin = []byte(os.Args[5])
+		humanoidMax = []byte(os.Args[6])
+
+		// TOOD: Make these args?
+		autopickPriority = []byte("DKQRTWX")
+		humanoidPriority = []byte("QRWX")
+	)
 	scanner := bufio.NewScanner(f)
 	out := csv.NewWriter(os.Stdout)
 	for scanner.Scan() {
 		roster := scanner.Text()
-		autopick := allowedPos(schema, []byte("DKQRTWX"), []byte(roster))
-		humanoid := allowedPos(schema, []byte("QRWX"), []byte(roster))
+		autopick := allowedPos(schema, autopickPriority, autopickMin, autopickMax, []byte(roster))
+		humanoid := allowedPos(schema, humanoidPriority, humanoidMin, humanoidMax, []byte(roster))
 		out.Write([]string{roster, autopick, humanoid})
 	}
 	if err := scanner.Err(); err != nil {
--- genrules/genrules_test.go
+++ genrules/genrules_test.go
@@ -4,9 +4,26 @@ import (
 	"testing"
 )
 
-func TestAllowedPos(t *testing.T) {
-	schema := []byte("QRRWWWTXDKBBBBBBBB")
+var (
+	schema           = []byte("QRRWWWTXDKBBBBBBBB")
+	autopickPriority = []byte("DKQRTWX")
+	autopickMin      = []byte("DKQQTTRRRRWWWW")
+	autopickMax      = []byte("DDKKQQTTTRRRRRRWWWWWW")
+	humanoidPriority = []byte("QRWX")
+	humanoidMin      = []byte("DKQQTTRRRRWWWW")
+	humanoidMax      = []byte("DDKQQTTTRRRRRRWWWWWW")
 
+	noMin = []byte("")
+	noMax = []byte("" +
+		"DDDDDDDDDDDDDDDDDD" +
+		"KKKKKKKKKKKKKKKKKK" +
+		"QQQQQQQQQQQQQQQQQQ" +
+		"RRRRRRRRRRRRRRRRRR" +
+		"TTTTTTTTTTTTTTTTTT" +
+		"WWWWWWWWWWWWWWWWWW")
+)
+
+func TestAllowedPosNoMinMax(t *testing.T) {
 	tests := []struct {
 		roster   string
 		autopick string
@@ -26,11 +43,44 @@ func TestAllowedPos(t *testing.T) {
 		{"DQRRRTWWWWWWWWWWW", "K", "K"},     // at end must pick K
 	}
 	for _, tt := range tests {
-		autopick := allowedPos(schema, []byte("DKQRTWX"), []byte(tt.roster))
+		autopick := allowedPos(schema, autopickPriority, noMin, noMax, []byte(tt.roster))
 		if autopick != tt.autopick {
 			t.Errorf("allowedPos(_, %s) = autopick %s; want %s", tt.roster, autopick, tt.autopick)
 		}
-		humanoid := allowedPos(schema, []byte("QRWX"), []byte(tt.roster))
+		humanoid := allowedPos(schema, humanoidPriority, noMin, noMax, []byte(tt.roster))
+		if humanoid != tt.humanoid {
+			t.Errorf("allowedPos(_, %s) = humanoid %s; want %s", tt.roster, humanoid, tt.humanoid)
+		}
+	}
+}
+
+func TestAllowedPosWithMinMax(t *testing.T) {
+	tests := []struct {
+		roster   string
+		autopick string
+		humanoid string
+	}{
+		{"", "DKQRTW", "DKQRTW"},
+		{"RR", "DKQRTW", "DKQRTW"},         // still an X roster free
+		{"RRR", "DKQTW", "DKQTW"},          // no more R
+		{"QRW", "DKRTW", "DKRTW"},          // no more Q
+		{"RRRWWWT", "DKQ", "DKQ"},          // must fill Q
+		{"DRRRWWWT", "KQ", "KQ"},           // must fill Q
+		{"QRRRTWWW", "DK", "DKQRTW"},       // autopick requires DK before others
+		{"DKQRRRWWW", "T", "DQRTW"},        // autopick requires T before others; humanoid one K
+		{"QRRRTWWWWWWWWWW", "DK", "DKQRT"}, // almost end; reached min W
+		{"QRRRTWWWWWWWWWWW", "DK", "DK"},   // at end must pick DK
+		{"KQRRRTWWWWWWWWWWW", "D", "D"},    // at end must pick D
+		{"DQRRRTWWWWWWWWWWW", "K", "K"},    // at end must pick K
+		{"DKQRRRRTTWWWWWWWW", "Q", "Q"},    // at end need min QQ
+		{"DDKQQRRRRTTWWWWWW", "KRT", "RT"}, // maxed out DQW and humanoid K
+	}
+	for _, tt := range tests {
+		autopick := allowedPos(schema, autopickPriority, autopickMin, autopickMax, []byte(tt.roster))
+		if autopick != tt.autopick {
+			t.Errorf("allowedPos(_, %s) = autopick %s; want %s", tt.roster, autopick, tt.autopick)
+		}
+		humanoid := allowedPos(schema, humanoidPriority, humanoidMin, humanoidMax, []byte(tt.roster))
 		if humanoid != tt.humanoid {
 			t.Errorf("allowedPos(_, %s) = humanoid %s; want %s", tt.roster, humanoid, tt.humanoid)
 		}
