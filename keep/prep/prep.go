80a35a97ee23c42bf6db8945bab80a72ffc7c803
David B. Tucker <dbtleonia@gmail.com>
2021-08-30 02:40:59 -0400

    Overhaul prep code to write projections.csv. (#6)

--- keep/prep/prep.go
+++ keep/prep/prep.go
@@ -4,61 +4,41 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"path"
+	"path/filepath"
+	"regexp"
 	"strings"
+	"unicode"
 )
 
 var (
-	// ===== Format 1 =====
+	// ===== Inputs under <data_dir> =====
 	//
-	// Inputs:
-	//   <data_dir>/managers.csv
-	//     field 0   = <manager>
-	//     field 2   = <gridder-name>
-	//     field 3   = (<team> - <pos>)
-	//     field 8   = <round>
-	//   <data_dir>/projections/<pos>.csv
-	//     field 0   = <gridder-name>
-	//     field 1   = <team>  # uppercase; empty for DEF.csv
-	//     field n-1 = <projection>
-	//   <data_dir>/missing.csv
-	//     field 0   = <gridder-name> (<team> - <pos>)
-	//   <data_dir>/renames.csv
-	//     field 0   = <gridder-name-projections>
-	//     field 1   = <gridder-name-managers>
-	// Output:
-	//   <data_dir>/merged.csv
+	// keeper_rounds.csv
+	//   field 0  = <manager>
+	//   field 1  = <player-name-canon>
+	//   field 2  = <round>
+	// raw3_projections.tsv  -- TSV, not CSV
+	//   field 1  = <player-name-format3>
+	//   field 2  = $<value>
+	// raw3_extra_renames.csv
+	//   field 0  = <player-name-format3>
+	//   field 1  = <player-name-canon>
+	// raw3_extra_projections.csv
+	//   field 0  = <player-name-canon>
 	//
-	// ===== Format 2 =====
+	// ===== Output under <data_dir> =====
 	//
-	// Inputs:
-	//   <data_dir>/managers.csv
-	//     field 0   = <manager>
-	//     field 2   = <gridder-name>
-	//     field 3   = (<team> - <pos>)
-	//     field 8   = <round>
-	//   <data_dir>/projections.csv
-	//     field 0   = <gridder-name>
-	//     field 1   = <pos>
-	//     field 2   = <team>  # uppercase
-	//     field 4   = <stddev>
-	//     field 8   = <projection>  # vor
-	//   <data_dir>/missing.csv
-	//     field 0   = <gridder-name> (<team> - <pos>)
-	//   <data_dir>/renames.csv
-	//     field 0   = <gridder-name-projections>
-	//     field 1   = <gridder-name-managers>
-	// Output:
-	//   <data_dir>/merged.csv
+	// projections.csv
+	//   field 0  = <player-name-canon>
+	//   field 1  = <value>
+	//   field 2  = <stddev>  # empty, future formats may populate
+	//
+	dataDir = flag.String("data_dir", "", "directory for data files; empty string means $HOME/data")
 
-	dataDir = flag.String("data_dir", "", "directory containing data sources")
-)
-
-var (
+	// Mapping of team formats from raw -> canon.
 	teams = map[string]string{
 		"ARI": "Ari",
 		"ATL": "Atl",
@@ -78,8 +58,7 @@ var (
 		"KC":  "KC",
 		"LAC": "LAC",
 		"LAR": "LAR",
-		"LV":  "LV", // Format 1
-		"LVR": "LV", // Format 2
+		"LV":  "LV",
 		"MIA": "Mia",
 		"MIN": "Min",
 		"NE":  "NE",
@@ -93,285 +72,123 @@ var (
 		"TB":  "TB",
 		"TEN": "Ten",
 		"WAS": "Was",
-		"FA":  "FA", // Format 2 (free agents)
+		"":    "",
 	}
 
-	// Format 1
-	defnames1 = map[string]string{
-		"Arizona Cardinals":        "Arizona (Ari - DEF)",
-		"Atlanta Falcons":          "Atlanta (Atl - DEF)",
-		"Baltimore Ravens":         "Baltimore (Bal - DEF)",
-		"Buffalo Bills":            "Buffalo (Buf - DEF)",
-		"Carolina Panthers":        "Carolina (Car - DEF)",
-		"Chicago Bears":            "Chicago (Chi - DEF)",
-		"Cincinnati Bengals":       "Cincinnati (Cin - DEF)",
-		"Cleveland Browns":         "Cleveland (Cle - DEF)",
-		"Dallas Cowboys":           "Dallas (Dal - DEF)",
-		"Denver Broncos":           "Denver (Den - DEF)",
-		"Detroit Lions":            "Detroit (Det - DEF)",
-		"Green Bay Packers":        "Green Bay (GB - DEF)",
-		"Houston Texans":           "Houston (Hou - DEF)",
-		"Indianapolis Colts":       "Indianapolis (Ind - DEF)",
-		"Jacksonville Jaguars":     "Jacksonville (Jax - DEF)",
-		"Kansas City Chiefs":       "Kansas City (KC - DEF)",
-		"Las Vegas Raiders":        "Las Vegas (LV - DEF)",
-		"Los Angeles Chargers":     "Los Angeles (LAC - DEF)",
-		"Los Angeles Rams":         "Los Angeles (LAR - DEF)",
-		"Miami Dolphins":           "Miami (Mia - DEF)",
-		"Minnesota Vikings":        "Minnesota (Min - DEF)",
-		"New England Patriots":     "New England (NE - DEF)",
-		"New Orleans Saints":       "New Orleans (NO - DEF)",
-		"New York Giants":          "New York (NYG - DEF)",
-		"New York Jets":            "New York (NYJ - DEF)",
-		"Philadelphia Eagles":      "Philadelphia (Phi - DEF)",
-		"Pittsburgh Steelers":      "Pittsburgh (Pit - DEF)",
-		"San Francisco 49ers":      "San Francisco (SF - DEF)",
-		"Seattle Seahawks":         "Seattle (Sea - DEF)",
-		"Tampa Bay Buccaneers":     "Tampa Bay (TB - DEF)",
-		"Tennessee Titans":         "Tennessee (Ten - DEF)",
-		"Washington Football Team": "Washington (Was - DEF)",
-	}
-
-	// Format 2
-	defnames2 = map[string]string{
-		"Cardinals":  "Arizona (Ari - DEF)",
-		"Falcons":    "Atlanta (Atl - DEF)",
-		"Ravens":     "Baltimore (Bal - DEF)",
-		"Bills":      "Buffalo (Buf - DEF)",
-		"Panthers":   "Carolina (Car - DEF)",
-		"Bears":      "Chicago (Chi - DEF)",
-		"Bengals":    "Cincinnati (Cin - DEF)",
-		"Browns":     "Cleveland (Cle - DEF)",
-		"Cowboys":    "Dallas (Dal - DEF)",
-		"Broncos":    "Denver (Den - DEF)",
-		"Lions":      "Detroit (Det - DEF)",
-		"Packers":    "Green Bay (GB - DEF)",
-		"Texans":     "Houston (Hou - DEF)",
-		"Colts":      "Indianapolis (Ind - DEF)",
-		"Jaguars":    "Jacksonville (Jax - DEF)",
-		"Chiefs":     "Kansas City (KC - DEF)",
-		"Raiders":    "Las Vegas (LV - DEF)",
-		"Chargers":   "Los Angeles (LAC - DEF)",
-		"Rams":       "Los Angeles (LAR - DEF)",
-		"Dolphins":   "Miami (Mia - DEF)",
-		"Vikings":    "Minnesota (Min - DEF)",
-		"Patriots":   "New England (NE - DEF)",
-		"Saints":     "New Orleans (NO - DEF)",
-		"Giants":     "New York (NYG - DEF)",
-		"Jets":       "New York (NYJ - DEF)",
-		"Eagles":     "Philadelphia (Phi - DEF)",
-		"Steelers":   "Pittsburgh (Pit - DEF)",
-		"49ers":      "San Francisco (SF - DEF)",
-		"Seahawks":   "Seattle (Sea - DEF)",
-		"Buccaneers": "Tampa Bay (TB - DEF)",
-		"Titans":     "Tennessee (Ten - DEF)",
-		"Redskins":   "Washington (Was - DEF)",
-	}
+	teamRE = regexp.MustCompile(`\([^ ]*`)
 )
+
+func translateName(playerRaw string) (string, error) {
+	// Remove extra chars to the right of ')'.
+	playerRaw = strings.TrimRightFunc(playerRaw, unicode.IsLetter)
+
+	// Replace '(<team-raw>' with '(<team-canon'.
+	var err error
+	result := teamRE.ReplaceAllStringFunc(playerRaw, func(teamRaw string) string {
+		teamRaw = teamRaw[1:] // strip '('
+		teamCanon, ok := teams[teamRaw]
+		if !ok {
+			err = fmt.Errorf("unknown team: %q in player %q", teamRaw, playerRaw)
+			return ""
+		}
+		return "(" + teamCanon
+	})
+
+	return result, err
+}
+
+func mustReadAll(filename string) [][]string {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	if filepath.Ext(filename) == ".tsv" {
+		r.Comma = '\t'
+	}
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return records
+}
 
 func main() {
 	flag.Parse()
 
-	// Open output file & write header.
-	out, err := os.Create(path.Join(*dataDir, "merged.csv"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	writer := csv.NewWriter(out)
-	header := []string{"player", "proj", "manager", "rd"}
-	if err := writer.Write(header); err != nil {
-		log.Fatal(err)
-	}
-
-	// Read renames.
-	renames := make(map[string]string)
-	rfile, err := os.Open(path.Join(*dataDir, "renames.csv"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rfile.Close()
-	rreader := csv.NewReader(rfile)
-	for {
-		record, err := rreader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		renames[record[0]] = record[1]
-	}
-
-	// Read missing.
-	missing := make(map[string]bool)
-	mfile, err := os.Open(path.Join(*dataDir, "missing.csv"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer mfile.Close()
-	mreader := csv.NewReader(mfile)
-	for {
-		record, err := mreader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		missing[record[0]] = true
-	}
-
-	// Read projections.
-	//
-	// Projections come in one of 2 formats.
-	_, err = os.Stat(path.Join(*dataDir, "projections.csv"))
-	isFormat1 := os.IsNotExist(err)
-
-	projections := make(map[string]string) // gridder name -> projected points
-	if isFormat1 {
-		pfiles, err := ioutil.ReadDir(path.Join(*dataDir, "projections"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, file := range pfiles {
-			pos := strings.TrimSuffix(file.Name(), ".csv")
-			f, err := os.Open(path.Join(*dataDir, "projections", file.Name()))
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer f.Close()
-
-			r := csv.NewReader(f)
-			r.Read() // skip header
-			for {
-				record, err := r.Read()
-				if err == io.EOF {
-					break
-				}
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				var bigname string
-				if record[1] == "" {
-					var ok bool
-					bigname, ok = defnames1[record[0]]
-					if !ok {
-						log.Fatalf("Unknown name: %q in record %v", record[0], record)
-					}
-				} else {
-					name := record[0]
-					team, ok := teams[record[1]]
-					if !ok {
-						log.Fatalf("Unknown team: %q in record %v", record[1], record)
-					}
-					bigname = fmt.Sprintf("%s (%s - %s)", name, team, pos)
-					if rename, ok := renames[bigname]; ok {
-						bigname = rename
-					}
-				}
-				projections[bigname] = record[len(record)-1]
-			}
-		}
-	} else {
-		pfile, err := os.Open(path.Join(*dataDir, "projections.csv"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer pfile.Close()
-		preader := csv.NewReader(pfile)
-		preader.Read() // skip header
-		for {
-			record, err := preader.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			var bigname string
-			if record[1] == "DST" {
-				var ok bool
-				bigname, ok = defnames2[record[0]]
-				if !ok {
-					log.Fatalf("Unknown name: %q in record %v", record[0], record)
-				}
-			} else {
-				name := record[0]
-				team, ok := teams[record[2]]
-				if !ok {
-					log.Fatalf("Unknown team: %q in record %v", record[2], record)
-				}
-				pos := record[1]
-				bigname = fmt.Sprintf("%s (%s - %s)", name, team, pos)
-				if rename, ok := renames[bigname]; ok {
-					bigname = rename
-				}
-			}
-			vor := record[8]
-			projections[bigname] = vor
+	dir := *dataDir
+	if dir == "" {
+		if home := os.Getenv("HOME"); home != "" {
+			dir = path.Join(home, "data")
 		}
 	}
 
-	// Read managers & write owned gridders.
-	mgrfile, err := os.Open(path.Join(*dataDir, "managers.csv"))
-	if err != nil {
-		log.Fatal(err)
+	// Read extra renames.
+	extraRenames := make(map[string]string)
+	for _, record := range mustReadAll(path.Join(dir, "raw3_extra_renames.csv")) {
+		extraRenames[record[0]] = record[1]
 	}
-	defer mgrfile.Close()
-	mgrreader := csv.NewReader(mgrfile)
-	mgrreader.Read() // skip header
 
-	for {
-		record, err := mgrreader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
+	// Read extra projections.
+	extraProjections := make(map[string]string) // player -> value
+	extraProjectionsOrder := []string{}
+	for _, record := range mustReadAll(path.Join(dir, "raw3_extra_projections.csv")) {
+		extraProjections[record[0]] = record[1]
+		extraProjectionsOrder = append(extraProjectionsOrder, record[0])
+	}
 
-		var (
-			managerName = record[0]
-			name        = record[2] + " " + record[3]
-			round       = record[8]
-		)
+	// Read raw projections.
+	projections := make(map[string]string) // player -> value
+	projectionsOrder := []string{}
+	for _, record := range mustReadAll(path.Join(dir, "raw3_projections.tsv")) {
+		playerRaw := record[1]
 
-		proj, ok := projections[name]
-		if !ok {
-			if !missing[name] {
-				log.Fatalf("No projection for %q\n", name)
-			}
-			if isFormat1 {
-				proj = "0.0"
-			} else {
-				proj = "-999.0"
-			}
+		var player string
+		if rename, ok := extraRenames[playerRaw]; ok {
+			player = rename
 		} else {
-			delete(projections, name)
-		}
-		if !isFormat1 && strings.Contains(name, "- K") {
-			proj = "-999.0"
+			var err error
+			player, err = translateName(playerRaw)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 
-		r := []string{name, proj, managerName, round}
-		if err := writer.Write(r); err != nil {
-			log.Fatal(err)
+		value := strings.TrimPrefix(record[2], "$")
+
+		projections[player] = value
+		projectionsOrder = append(projectionsOrder, player)
+	}
+
+	// Read keeper_rounds and check constraints.
+	krecords := mustReadAll(path.Join(dir, "keeper_rounds.csv"))
+	for _, record := range krecords[1:] { // skip header
+		name := record[1]
+		_, ok1 := projections[name]
+		_, ok2 := extraProjections[name]
+		if !ok1 && !ok2 {
+			log.Fatalf("No projection for %q; add to extra renames or projections\n", name)
+		}
+		if ok1 && ok2 {
+			log.Fatalf("Multiple projections for %q; remove from extra projections\n", name)
 		}
 	}
 
-	// Write un-owned gridders.
-	for name, proj := range projections {
-		r := []string{name, proj, "", ""}
-		if err := writer.Write(r); err != nil {
-			log.Fatal(err)
-		}
+	// Write output file.
+	out := [][]string{{"player", "value", "stddev"}}
+	for _, name := range projectionsOrder {
+		out = append(out, []string{name, projections[name], ""})
 	}
-
-	// Close output file.
-	writer.Flush()
-	if err := writer.Error(); err != nil {
+	for _, name := range extraProjectionsOrder {
+		out = append(out, []string{name, extraProjections[name], ""})
+	}
+	f, err := os.Create(path.Join(dir, "projections.csv"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := csv.NewWriter(f).WriteAll(out); err != nil {
 		log.Fatal(err)
 	}
 }
