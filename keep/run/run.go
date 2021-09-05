b240aa6eff6614826e95df782e249189f80a3e8e
David B. Tucker <dbtleonia@gmail.com>
2021-09-05 02:44:37 -0400

    Add --charts flag to run command. (#10)

--- keep/run/run.go
+++ keep/run/run.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"os"
 	"path"
@@ -12,8 +13,44 @@ import (
 )
 
 var (
+	charts  = flag.Bool("charts", true, "output HTML with charts of results")
 	dataDir = flag.String("data_dir", "", "directory for data files; empty string means $HOME/data")
 )
+
+func printCharts(c *constants, profile []action) {
+	nokeep := make([]action, len(profile))
+
+	// TODO: Translate to short manager names.
+	managerNames := make([]string, len(c.managers))
+	for i, m := range c.managers {
+		managerNames[i] = m.name[:9]
+	}
+	managersPre := utilityAll(c, nokeep)
+	managersPost := utilityAll(c, profile)
+
+	var pickNames []string
+	utilityAccum(c, nokeep, func(pick int, mid managerid, gid gridderid, iskeep bool) {
+		round := pick/len(profile) + 1
+		pickNames = append(pickNames, fmt.Sprintf("%s %d-%d", managerNames[mid], round, pick+1))
+	})
+	picksPre := pickValues(c, nokeep)
+	picksPost := pickValues(c, profile)
+	keepers := pickKeepers(c, profile)
+
+	tmpl := template.Must(template.ParseFiles("tmpl.html"))
+	data := map[string]interface{}{
+		"managerNames": managerNames,
+		"managersPre":  managersPre,
+		"managersPost": managersPost,
+		"pickNames":    pickNames,
+		"picksPre":     picksPre,
+		"picksPost":    picksPost,
+		"keepers":      keepers,
+	}
+	if err := tmpl.Execute(os.Stdout, data); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func ReadConstants(dataDir string) (*constants, error) {
 	g, err := os.Open(path.Join(dataDir, "projections.csv"))
@@ -97,6 +134,12 @@ func main() {
 		log.Fatal(err)
 	}
 	profiles := iteratedProfiles(consts)
+
+	// TODO: Pull this out into a separate command.
+	if *charts {
+		printCharts(consts, profiles[len(profiles)-1])
+		return
+	}
 
 	// Output the results.  This currently loops through the entire
 	// response for each gridder.  We could make it more efficient if
