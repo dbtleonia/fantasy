68536b8512030fd9da374e03a563d5243cd73721
David B. Tucker <dbtleonia@gmail.com>
2021-09-05 13:07:25 -0700

    Rewrite genplayers to use new projections format.

--- genplayers/genplayers.go
+++ genplayers/genplayers.go
@@ -4,128 +4,79 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
-
-var (
-	nullADPCutoff    = flag.Int("null_adp_cutoff", 100, "fatal error if player with null ADP has rank less than cutoff")
-	missingByeCutoff = flag.Int("missing_bye_cutoff", 10, "fatal error if more than this number of players is missing a bye")
-)
-
-func readByes(filename string) (map[string]string, error) {
-	const (
-		colName = 0
-		colPos  = 1
-		colTeam = 2
-		colBye  = 3
-	)
-	f, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	in := csv.NewReader(f)
-	if _, err = in.Read(); err != nil { // discard the first line
-		return nil, err
-	}
-	byes := make(map[string]string) // key is <name><pos><team>
-	for {
-		record, err := in.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-		key := record[colName] + record[colPos] + record[colTeam]
-		byes[key] = record[colBye]
-	}
-	return byes, nil
-}
 
 func main() {
 	flag.Parse()
-	if flag.NArg() < 1 || flag.NArg() > 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <custom-rankings-csv> [<raw-stat-projections-csv>]", os.Args[0])
+	if flag.NArg() != 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <projections-tsv>", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-	var byes map[string]string // key is <name><pos><team>
-	if flag.NArg() == 2 {
-		var err error
-		byes, err = readByes(flag.Arg(1))
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	const (
-		colID      = 0
-		colName    = 1
-		colPos     = 3
-		colTeam    = 2
-		colVOR     = 20
-		colPoints  = 19
-		colRank    = 23
-		colPosRank = 24
-		colADP     = 29 // alternative is ECR in col 13
-		colCeiling = 22
-	)
+
 	f, err := os.Open(flag.Arg(0))
 	if err != nil {
 		log.Fatal(err)
 	}
 	in := csv.NewReader(f)
-	if _, err = in.Read(); err != nil { // discard the first line
+	in.Comma = '\t'
+	records, err := in.ReadAll()
+	if err != nil {
 		log.Fatal(err)
 	}
-	out := csv.NewWriter(os.Stdout)
-	missingByeCount := 0
-	for {
-		record, err := in.Read()
-		if err == io.EOF {
-			break
+
+	const (
+		colName   = 1
+		colPoints = 2
+		colValue  = 3
+	)
+	var out [][]string
+	var problems []string
+	poscount := make(map[string]int)
+	for i, record := range records {
+		var pos string
+		switch {
+		case strings.Contains(record[colName], "- DST"):
+			pos = "DST"
+		case strings.Contains(record[colName], "- K"):
+			pos = "K"
+		case strings.Contains(record[colName], "- QB"):
+			pos = "QB"
+		case strings.Contains(record[colName], "- RB"):
+			pos = "RB"
+		case strings.Contains(record[colName], "- TE"):
+			pos = "TE"
+		case strings.Contains(record[colName], "- WR"):
+			pos = "WR"
+		default:
+			problems = append(problems, record[colName])
 		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		if record[colADP] == "NA" {
-			rank, err := strconv.Atoi(record[colRank])
-			if err != nil {
-				log.Fatal(err)
-			}
-			if rank < *nullADPCutoff {
-				log.Fatalf("Player with rank %d (below cutoff %d) has null ADP: %v", rank, *nullADPCutoff, record)
-			}
-			continue // these players ain't gonna be drafted anyhow
-		}
-		var bye string
-		if byes != nil {
-			key := record[colName] + record[colPos] + record[colTeam]
-			var ok bool
-			bye, ok = byes[key]
-			if !ok {
-				missingByeCount++
-			}
-		}
-		out.Write([]string{
+		poscount[pos]++
+
+		// TODO: Remove dummy values once sim/opt no longer need them.
+		out = append(out, []string{
 			"0",
-			record[colID],
-			record[colName],
-			record[colPos],
-			record[colTeam],
-			record[colVOR],
-			record[colPoints],
-			record[colRank],
-			record[colPosRank],
-			record[colADP],
-			record[colCeiling],
-			bye,
+			strconv.Itoa(10000 + i), // id
+			record[colName],         // name
+			pos,                     // pos
+			"XXX",                   // team
+			strings.TrimPrefix(record[colValue], "$"), // value
+			record[colPoints],                         // points
+			strconv.Itoa(i + 1),                       // rank
+			strconv.Itoa(poscount[pos]),               // posrank
+			strconv.Itoa(i + 1),                       // adp
+			strings.TrimPrefix(record[colValue], "$"), // ceiling
+			"", // bye
 		})
 	}
-	out.Flush()
-	if missingByeCount > *missingByeCutoff {
-		log.Fatalf("Found %d players missing byes, more than cutoff %d", missingByeCount, *missingByeCutoff)
+	if len(problems) > 0 {
+		log.Fatalf("Unknown positions:  \n  %s\n", strings.Join(problems, "\n  "))
+	}
+	if err = csv.NewWriter(os.Stdout).WriteAll(out); err != nil {
+		log.Fatal(err)
 	}
 }
