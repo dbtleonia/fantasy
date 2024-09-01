ca602d9985d533a0dcfb37b1f54bd5f8a7471991
David B. Tucker <dbtleonia@gmail.com>
2024-09-01 10:55:41 -0700

    Read draft order from same file used for keepers.

--- order.go
+++ order.go
@@ -15,6 +15,10 @@ func ReadOrder(orderCsv string) ([]int, error) {
 	}
 	order := []int{8888} // dummy as first element; picks start at 1
 	r := csv.NewReader(f)
+	if _, err := r.Read(); err != nil { // discard header
+		return nil, err
+	}
+	teams := make(map[string]int)
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -30,9 +34,10 @@ func ReadOrder(orderCsv string) ([]int, error) {
 		if pick != len(order) {
 			return nil, fmt.Errorf("got pick %d, want %d", pick, len(order))
 		}
-		team, err := strconv.Atoi(record[2])
-		if err != nil {
-			return nil, err
+		team, ok := teams[record[1]]
+		if !ok {
+			team = len(teams)
+			teams[record[1]] = team
 		}
 		order = append(order, team)
 	}
